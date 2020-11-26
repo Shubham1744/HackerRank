53f8679c795d5066f0c68ac429367d835cae7498
Shubham1744 <66161577+Shubham1744@users.noreply.github.com>
2020-11-26 18:45:34 +0530

    solution

---
+++ 30_Days_of_code/Day_1_Data_Types/Solution.py
@@ -0,0 +1,27 @@
+
+
+int_2 = int(input())
+double_2 = float(input())
+string_2 = input()
+
+# Read and save an integer, double, and String to your variables.
+sum_int = i + int_2
+sum_double = d + double_2
+sum_string = s + string_2
+
+# Print the sum of both integer variables on a new line.
+# Print the sum of the double variables on a new line.
+# Concatenate and print the String variables on a new line
+print(sum_int)
+print(sum_double)
+print(sum_string)
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
